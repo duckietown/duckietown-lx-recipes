6dda4a0e37598aefbc8a134d03e49a796f44b747
davide <Tuxliri@users.noreply.github.com>
2023-03-18 14:14:42 +0100

    Improved gains loader

--- dd21-pid-tuning-recipe/packages/utils/writer.py
+++ dd21-pid-tuning-recipe/packages/utils/writer.py
@@ -2,9 +2,15 @@ import yaml
 
 def update_gains(kp,kd,ki,k=0,filepath = ""):
     with open(filepath,"w") as f:
-        gains = {'Kp':kp,'Kd':kd,'Ki':ki,'K':k}
-        yaml.dump(gains,f)
-        f.close()
+        try:
+            gains = {'Kp':kp,'Kd':kd,'Ki':ki,'K':k}
+            yaml.dump(gains,f)
+            f.close()
+
+        except yaml.YAMLError as exc:
+            print(exc)
+            print(f'Failed to load PID terms from {filepath}')
+
 
 def load_gains(filepath)  -> dict:
     with open(filepath,"r") as f:
