93f3ab3be9ceeab36fe2872e5b8375f8405ed4fe
davide <Tuxliri@users.noreply.github.com>
2023-03-18 14:10:17 +0100

    Changed nomenclature

--- dd21-pid-tuning-recipe/packages/utils/writer.py
+++ dd21-pid-tuning-recipe/packages/utils/writer.py
@@ -1,13 +1,13 @@
 import yaml
 
-def update_gains(kp,kd,ki,filepath):
+def update_gains(kp,kd,ki,k=0,filepath = ""):
     with open(filepath,"w") as f:
-        gains = {'kp':kp,'kd':kd,'ki':ki}
+        gains = {'Kp':kp,'Kd':kd,'Ki':ki,'K':k}
         yaml.dump(gains,f)
         f.close()
 
-def load_gains(filepath):
+def load_gains(filepath)  -> dict:
     with open(filepath,"r") as f:
         gains = yaml.full_load(f)
         f.close()
-    return gains['kp'], gains['kd'], gains['ki']
+    return gains
