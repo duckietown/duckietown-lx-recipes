09bd148af13737808894768fbba5f135458a78d5
davide <Tuxliri@users.noreply.github.com>
2023-04-06 13:45:52 -0600

    Simulator working

---
+++ dd21-ukf-recipe/assets/vnc/usr/bin/launch-ukf-2d.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/duckie/catkin_ws/devel/setup.bash
+roslaunch --wait rosbridge_server rosbridge_websocket.launch&
+python /home/duckie/catkin_ws/src/pidrone_pkg/scripts/state_estimator.py --primary ukf2d --others simulator --ir_var 0.01
