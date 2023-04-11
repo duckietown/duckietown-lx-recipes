6268835c5d9acf0495457f4affcca78dc56e5b1f
davide <Tuxliri@users.noreply.github.com>
2023-04-11 12:11:37 -0600

    Added IR_VARIANCE_ESTIMATE input to simulator

--- dd21-ukf-recipe/assets/vnc/usr/bin/launch-ukf-2d.sh
+++ dd21-ukf-recipe/assets/vnc/usr/bin/launch-ukf-2d.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/duckie/catkin_ws/devel/setup.bash
 roslaunch --wait rosbridge_server rosbridge_websocket.launch&
-python /home/duckie/catkin_ws/src/pidrone_pkg/scripts/state_estimator.py --primary ukf2d --others simulator --ir_var 0.01
+python /home/duckie/catkin_ws/src/pidrone_pkg/scripts/state_estimator.py --primary ukf2d --others simulator --ir_var $IR_VARIANCE_ESTIMATE --student
