36f109ed8a5a29112aa33f05e50a16e36dcc500b
davide <Tuxliri@users.noreply.github.com>
2023-04-12 13:19:02 -0600

    DTSW-3137 fixed desktop links to simulators

--- dd21-ukf-recipe/assets/vnc/usr/bin/launch-ukf-2d.sh
+++ dd21-ukf-recipe/assets/vnc/usr/bin/launch-ukf-2d.sh
@@ -4,4 +4,4 @@ source /root/.bashrc
 source /home/duckie/catkin_ws/devel/setup.bash
 source /root/catkin_ws/devel/setup.bash
 roslaunch --wait rosbridge_server rosbridge_websocket.launch&
-python state_estimator.py --student --primary ukf2d --others simulator --ir_var $IR_VARIANCE_ESTIMATE
+python /home/duckie/catkin_ws/src/pidrone_pkg/scripts/state_estimator.py --student --primary ukf2d --others simulator --ir_var $IR_VARIANCE_ESTIMATE
