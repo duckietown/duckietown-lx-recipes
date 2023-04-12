dd1b36c13fb5c521a4b44369caeb11dfb34fd9c9
davide <Tuxliri@users.noreply.github.com>
2023-04-12 13:41:37 -0600

    Fixed python error at startup

--- dd21-ukf-recipe/packages/__solution__/main.py
+++ dd21-ukf-recipe/packages/__solution__/main.py
@@ -1,6 +1,9 @@
+import time
 def main():
     # TODO: implement call to exercise solution from the `meat` directory here
-    print("\nERROR: Exercise `__solution__` was not implemented.\n")
+    print("Going")
+    time.sleep(10000)
+    # print("\nERROR: Exercise `__solution__` was not implemented.\n")
 
 
 if __name__ == "__main__":
