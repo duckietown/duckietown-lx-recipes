e7587b0dbf13836a35fd0b081cdcac59c8fdb3e9
kghite <kathryn@duckietown.com>
2023-05-21 12:05:40 -0400

    Publish led lx

--- led-control-recipe/assets/vnc/root/Documents/tk-window.py
+++ led-control-recipe/assets/vnc/root/Documents/tk-window.py
@@ -9,15 +9,15 @@ p1 = tk.PhotoImage(file="/root/.icons/duckietown.png")
 root.iconphoto(False, p1)
 
 # setting the windows size
-root.geometry("250x150")
+root.geometry("600x200")
 root.title("VLS control tool")
 
 DB_NAME = os.getenv("VEHICLE_NAME")
 
 # Create a frame to center the window message
-frame = tk.Frame(root, width=300, height=300)
+frame = tk.Frame(root, width=550, height=150)
 frame.grid(row=0, column=0, sticky="NW")
-label = tk.Label(root, text=f"Success! Your LEDs are updating on {DB_NAME}", font='Arial 18 bold')
+label = tk.Label(root, text=f"Success! LEDs updating on {DB_NAME}", font='Arial 14 bold')
 label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 root.mainloop()
--- led-control-recipe/packages/led_publisher/src/led_publisher_node.py
+++ led-control-recipe/packages/led_publisher/src/led_publisher_node.py
@@ -13,26 +13,30 @@ class LedPublisherNode(DTROS):
     def __init__(self, node_name):
         # Initialize the DTROS parent class
         super(LedPublisherNode, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
-        self.log("Initializing...")
+        self.log("Initializing ...")
+
+        # Determine what Duckiebot the node is running on
+        self.veh = rospy.get_namespace().strip("/")
 
         # Start ping subscriber
-        rospy.Subscriber(f"/{self.veh}/led_node/update", Bool, self.fade_through_colors(), queue_size=1)
+        rospy.Subscriber(f"/{self.veh}/led_node/update", Bool, self.fade_through_colors, queue_size=1)
 
         # LED publisher
         self.pub_leds = rospy.Publisher(
-            "~led_pattern",
+            f"/{self.veh}/led_emitter_node/led_pattern",
             LEDPattern,
             queue_size=1
         )
         self.log("Initialized.")
 
     def fade_through_colors(self, msg):
-        if msg.data:
-            for color in ["red", "green", "blue", "white"]:
-                for intensity in [0.25, 0.5, 0.75, 1.0]:
-                    message: LEDPattern = publish_leds.set_leds_color(color, intensity)
-                    self.pub_leds.publish(message)
-                    rospy.sleep(1.)
+        self.log("Publishing to the LEDs ...")
+        for color in ["red", "green", "blue", "white"]:
+            for intensity in [0.25, 0.5, 0.75, 1.0]:
+                message: LEDPattern = publish_leds.set_leds_color(color, intensity)
+                self.pub_leds.publish(message)
+                rospy.sleep(3.)
+        self.log("Done!")
 
     def on_shutdown(self):
         super(LedPublisherNode, self).on_shutdown()
